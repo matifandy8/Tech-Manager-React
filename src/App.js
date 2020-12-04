1a8a796e9e10e681ed917df18513d4ca35af1dd7
matifandy <matifandy@gmail.com>
2020-12-04 19:50:54 -0300

    some style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import Jugadores from "./components/Jugadores";
 import EquipoSeleccionado from "./components/EquipoSeleccionado";
+import "./style.css";
 
 function App() {
   return (
--- src/components/EquipoSeleccionado.jsx
+++ src/components/EquipoSeleccionado.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Suplentes from './Suplentes'
 import Titulares from './Titulares'
+import '../equipoSeleccionado.css'
 
 function EquipoSeleccionado() {
     return (
         <div className="equiposeleccionado">
-            <Titulares />
-            <Suplentes />
+            <div className="equiposeleccionado__titulares">
+               <Titulares />
+            </div>
+            <div className="equiposeleccionado__suplentes">
+                <Suplentes />
+            </div>
+            
+            
         </div>
     )
 }
